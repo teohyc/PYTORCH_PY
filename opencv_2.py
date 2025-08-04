49eed706a3139a5b1ee07e6ed7c10a31b818d02a
teohyc2018@gmail.com <you@example.com>
2025-08-04 19:48:46 +0800

    renamed

--- opencv_2.py
+++ opencv_2.py
@@ -28,7 +28,7 @@ if torch.cuda.is_available():
 
 
 
-#conver to tensors and normalize
+#convert to tensors and normalize
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5,), (0.5,)) # mean and std
@@ -119,5 +119,139 @@ class MLP(nn.Module):
 #instatiate model
 mlp_model = MLP(num_classes = 10)
 
-#dummy input of (B,C,H,W) = (1,1,28,28)
+#dummy input of (B,C,H,W) = (1,1,28,28) #dispaly model summary
 print(summary(mlp_model, input_size = (1,1,28,28), row_settings = ["var_names"]))
+
+#training configuration
+criterion = F.nll_loss #negative log likelihood loss
+optimizer = optim.Adam(mlp_model.parameters(), lr = 1e-2)
+num_epochs = 40
+
+#check on gpu
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {DEVICE}")
+
+#model training
+def train(model, trainloader, criterion, optimizer, DEVICE):
+    
+    model.train()
+    model.to(DEVICE)
+    running_loss = 0
+    correct_predictions = 0
+    total_samples = 0
+
+    for images, labels in trainloader:
+        images, labels = images.to(DEVICE), labels.to(DEVICE)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        _, predicted = torch.max(outputs.data, dim=1)
+        total_samples += labels.size(0)
+        correct_predictions += (predicted == labels).sum().item()
+
+    avg_loss = running_loss / len(trainloader)
+    accuracy = 100 * correct_predictions / total_samples
+
+    return avg_loss, accuracy
+
+
+def validation(model, val_loader, criterion, DEVICE):
+
+    model.eval()
+    model.to(DEVICE)
+
+    running_loss = 0
+    correct_predictions = 0
+    total_samples = 0
+
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images, labels = images.to(DEVICE), labels.to(DEVICE)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1) #(B, class_id)
+            total_samples += labels.size(0)
+            correct_predictions += (predicted == labels).sum().item()
+
+    avg_loss = running_loss / len(val_loader)
+    accuracy = 100 * correct_predictions / total_samples
+
+    return avg_loss, accuracy
+
+#main 
+def main(model, trainloader, val_loader, epochs=5, DEVICE = "cuda"):
+
+    train_losses, val_losses = [], []
+    train_accuracies, val_accuracies = [], []
+
+    for epoch in range(epochs):
+        train_loss, train_accuracy = train(model, trainloader, criterion, optimizer, DEVICE)
+        val_loss, val_accuracy = validation(model, val_loader, criterion, DEVICE)
+
+        train_losses.append(train_loss)
+        train_accuracies.append(train_accuracy)
+        val_losses.append(val_loss)
+        val_accuracies.append(val_accuracy)
+
+        print(f"Epoch {epoch+1:0>2}/{epochs} - Train Loss: {train_loss:.4f}, Train Accuracy: {train_accuracy:.2f}% - Val Loss: {val_loss:.4f}, Val Accuracy: {val_accuracy:.2f}%")
+    
+    #plotting
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(range(1, epochs +1), train_losses, label='Train Loss')
+    plt.plot(range(1, epochs +1), val_losses, label='Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(range(1, epochs + 1), train_accuracies, label='Train Accuracy')
+    plt.plot(range(1, epochs + 1), val_accuracies, label='Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
+
+main(mlp_model, train_loader, val_loader, epochs = num_epochs, DEVICE = DEVICE)
+
+
+
+
+#for test images
+images, gt_labels = next(iter(val_loader))
+
+rand_idx = random.choice(range(len(images)))
+
+plt.imshow(images[rand_idx].squeeze())
+plt.title("Ground Truth Label: " + str(int(gt_labels[rand_idx])), fontsize = 12)
+plt.axis("off")
+plt.show()
+
+#formatting
+bold = f"\033[1m"
+reset = f"\033[0m"
+
+mlp_model.eval()
+
+with torch.no_grad():
+    batch_outputs = mlp_model(images.to(DEVICE))
+
+prob_score_batch = batch_outputs.softmax(dim=1).cpu()
+
+prob_score_test_image = prob_score_batch[rand_idx]
+pred_cls_id = prob_score_test_image.argmax()
+
+print("Predictions for each class on the test image\n")
+
+for idx, cls_prob in enumerate(prob_score_test_image):
+    if idx ==pred_cls_id:
+        print(f"{bold}Class ID: {idx} - {class_mapping[idx]}, Probability:{cls_prob:.3f}{reset}")
+    else:
+        print(f"Class ID {idx} - {class_mapping[idx]}, Probanility :{cls_prob:.3f}")
+
+
